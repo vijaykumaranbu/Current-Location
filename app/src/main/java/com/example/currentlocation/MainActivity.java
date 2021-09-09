e807744b1bfa5cd63273a5f9c4297e55af830c81
vijayakumaranbu <vijayakumaranbu2000@gmail.com>
2021-09-09 13:38:08 +0530

    commit

---
+++ app/src/main/java/com/example/currentlocation/MainActivity.java
@@ -0,0 +1,163 @@
+package com.example.currentlocation;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends FragmentActivity {
+
+    private LocationRequest locationRequest;
+    private Double latitude,longitude;
+    private static final int REQUEST_C0DE_LOCATION_PERMISSION = 1001;
+    private static final int REQUEST_CODE_CHECK_GPS = 1002;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getCurrentLocation();
+    }
+
+    private void getCurrentLocation() {
+        if(ActivityCompat.checkSelfPermission(
+                getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(
+                    MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_C0DE_LOCATION_PERMISSION
+            );
+        }
+        else{
+            locationRequest = LocationRequest.create();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            locationRequest.setInterval(5000);
+            locationRequest.setFastestInterval(2000);
+            if(isGPSEnabled()){
+                LocationServices.getFusedLocationProviderClient(MainActivity.this)
+                        .requestLocationUpdates(locationRequest, new LocationCallback() {
+                            @Override
+                            public void onLocationResult(@NonNull LocationResult locationResult) {
+                                super.onLocationResult(locationResult);
+                                LocationServices.getFusedLocationProviderClient(MainActivity.this)
+                                        .removeLocationUpdates(this);
+                                if(locationResult.getLocations().size() > 0){
+                                    int index = locationResult.getLocations().size() - 1;
+                                    latitude = locationResult.getLocations().get(index).getLatitude();
+                                    longitude = locationResult.getLocations().get(index).getLongitude();
+                                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                                            .findFragmentById(R.id.googleMap);
+                                    assert mapFragment != null;
+                                    mapFragment.getMapAsync(googleMap -> {
+                                        LatLng latLng = new LatLng(latitude,longitude);
+                                        MarkerOptions markerOptions = new MarkerOptions().position(latLng)
+                                                .title("Your Location");
+                                        // animate camera zoom location
+                                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                                        // Add marker
+                                        googleMap.addMarker(markerOptions);
+                                        // Circle around user location
+                                        CircleOptions circleOptions = new CircleOptions().center(latLng)
+                                                .fillColor(R.color.blue).radius(1000);
+                                        googleMap.addCircle(circleOptions);
+                                    });
+                                }
+                            }
+                        }, Looper.getMainLooper());
+            }
+            else
+                turnOnGPS();
+        }
+    }
+
+    private void turnOnGPS() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getApplicationContext())
+                .checkLocationSettings(builder.build());
+        result.addOnCompleteListener(task -> {
+            try {
+                LocationSettingsResponse response = task.getResult(ApiException.class);
+                showToast("GPS is on");
+            } catch (ApiException e) {
+                switch (e.getStatusCode()){
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                        try {
+                            resolvableApiException.startResolutionForResult(MainActivity.this,REQUEST_CODE_CHECK_GPS);
+                        } catch (IntentSender.SendIntentException sendIntentException) {
+                            sendIntentException.printStackTrace();
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        break;
+                }
+            }
+        });
+    }
+
+    private boolean isGPSEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_CHECK_GPS) {
+            switch (resultCode) {
+                case RESULT_OK:
+                    getCurrentLocation();
+                    break;
+                case RESULT_CANCELED:
+                    showToast("GPS required to be turn on");
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_C0DE_LOCATION_PERMISSION && grantResults.length > 0){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                getCurrentLocation();
+            else
+                showToast("Permission Dined");
+        }
+    }
+}
